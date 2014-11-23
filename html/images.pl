916a7f5e107de43945d1c19b7e427527e6bc84b1
Geoffrey De Belie <computergeoffrey@zoho.com>
2014-11-23 14:33:53 +0100

    HTML version of the manual (based on November 22, 2014 version)

---
+++ html/images.pl
@@ -0,0 +1,18 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate images original text with physical files.
+
+
+$key = q/|;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|2#2|; 
+
+$key = q/includegraphics[width=0.8linewidth]{screen-shotsslashevince};LFS=12;AAT/;
+$cached_env_img{$key} = q|3#3|; 
+
+$key = q/includegraphics[width=0.8linewidth]{screen-shotsslashsynaptic1};LFS=12;AAT/;
+$cached_env_img{$key} = q|4#4|; 
+
+$key = q/includegraphics[width=0.7linewidth]{.slashFinalLogo};LFS=12;AAT/;
+$cached_env_img{$key} = q|1#1|; 
+
+1;
+
---
+++ html/internals.pl
@@ -0,0 +1,126 @@
+# LaTeX2HTML 2012 (1.2)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_uxterm/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_CC-BY-SA-4/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_xterm/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_PAE/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mkUSB-Quick-Start-manual/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Gparted/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_PAE2/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_UEFILinux/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_apt/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mkUSB-quick-start-manual-nox/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_OBI_Quickstart/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_SliM/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_synaptic/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mkUSB-quick-start-manual-bas/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_unetbootin2/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_Evince/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_VirtualBox/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bugs2/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_seamonkey/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_OBI/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_unetbootin/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/URL_REF/;
+$ref_files{$key} = "$dir".q|node53.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/PDF_References/;
+$ref_files{$key} = "$dir".q|node54.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/synaptic/;
+$ref_files{$key} = "$dir".q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ToriOS/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_mkusb-url/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_ToriOS_ISO/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nodm/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_bugs/;
+$ref_files{$key} = "$dir".q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Testing/;
+$ref_files{$key} = "$dir".q|node49.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
