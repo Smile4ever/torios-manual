96a2b8b697d6e4cfcd107c7b262c6136f9b03b48
Paul <zleap@zleap.net>
2015-01-18 21:56:37 +0000

    graphics file tidy up

--- uploadmanual.sh
+++ uploadmanual.sh
@@ -1,3 +1,15 @@
+# making index
+makeindex torios.idx
+makeindex torios-testing-manual.idx
+# echo add to git hub
+git add torios-testing-manual.pdf
+git add torios.tex
 git add torios.pdf
-git commit -m "torios mamual"
+git add torios-testing-manual.pdf
+git add *.png
+cd screen-shots
+cd ..
+git add *.png
+git commit -m "torios mamuals"
 git push
+echo "done" 
